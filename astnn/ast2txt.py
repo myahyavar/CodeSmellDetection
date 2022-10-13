0980872ef08eaced41832153a85eaac00e70cc0f
M. Yahya Avar <89209381+myahyavar@users.noreply.github.com>
2022-10-13 11:51:11 +0300

    minor renaming

--- astnn/ast2txt.py
+++ astnn/ast2txt.py
@@ -3,4 +3,4 @@ f = open('data/ast.pkl', 'rb')   # 'r' for reading; can be omitted
 mydict = pickle.load(f)         # load file content as mydict
 f.close()
 print(mydict)
-mydict.to_csv(r'txt/ast.txt', header=None, index=None, sep=' ', mode='a')
+mydict.to_csv(r'textfiles/ast.txt', header=None, index=None, sep=' ', mode='a')
