e6c9e1a44aa666c226705eb7d1e182f3bdb6e9b8
M. Yahya Avar <89209381+myahyavar@users.noreply.github.com>
2023-05-25 03:54:38 +0300

    cleanup

--- astnn-master/csv2pkl.py
+++
@@ -1,4 +0,0 @@
-import pickle
-import pandas as pd
-df = pd.read_csv('data/codes.csv')
-df.to_pickle('data/ast.pkl')
--- code2vec-master/Input.java
+++
@@ -1,8 +0,0 @@
-int f(int n) {
-    int i= n+56;
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * f(n-1);
-    }
-}
